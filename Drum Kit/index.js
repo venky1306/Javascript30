a8a9beae6b1c035c8ffc76d4f9e7a2b6e52f5b77
venky1306 <47674519+venky1306@users.noreply.github.com>
2022-12-25 22:10:20 -0500

    added sounds for key strokes.

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -12,46 +12,64 @@ function checkKeyPressed(evt) {
     if (evt.keyCode == "65") {
         console.log("You pressed 'A'.");
         var element = document.getElementById("a");
-        trans(element)
+        trans(element);
+        const aud = new Audio("sounds/clap.wav");
+        aud.play();
     }
     if (evt.keyCode == "83") {
         console.log("You pressed 'S'.");
         var element = document.getElementById("s");
-        trans(element)
+        trans(element);
+        const aud = new Audio("sounds/hihat.wav");
+        aud.play();
     }
     if (evt.keyCode == "68") {
         console.log("You pressed 'D'.");
         var element = document.getElementById("d");
-        trans(element)
+        trans(element);
+        const aud = new Audio("sounds/kick.wav");
+        aud.play();
     }
     if (evt.keyCode == "70") {
         console.log("You pressed 'F'.");
         var element = document.getElementById("f");
-        trans(element)
+        trans(element);
+        const aud = new Audio("sounds/openhat.wav");
+        aud.play();
     }
     if (evt.keyCode == "71") {
         console.log("You pressed 'G'.");
         var element = document.getElementById("g");
-        trans(element)
+        trans(element);
+        const aud = new Audio("sounds/boom.wav");
+        aud.play();
     }
     if (evt.keyCode == "72") {
         console.log("You pressed 'H'.");
         var element = document.getElementById("h");
-        trans(element)
+        trans(element);
+        const aud = new Audio("sounds/ride.wav");
+        aud.play();
     }
     if (evt.keyCode == "74") {
         console.log("You pressed 'J'.");
         var element = document.getElementById("j");
-        trans(element)
+        trans(element);
+        const aud = new Audio("sounds/snare.wav");
+        aud.play();
     }
     if (evt.keyCode == "75") {
         console.log("You pressed 'K'.");
         var element = document.getElementById("k");
-        trans(element)
+        trans(element);
+        const aud = new Audio("sounds/tom.wav");
+        aud.play();
     }
     if (evt.keyCode == "76") {
         console.log("You pressed 'L'.");
         var element = document.getElementById("l");
-        trans(element)
+        trans(element);
+        const aud = new Audio("sounds/tink.wav");
+        aud.play();
     }
 }
